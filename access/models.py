1f110f6736762c642051a477b9e5fc2145b1769b
Nikita <nikitasizpl@gmail.com>
2026-01-15 00:58:03 +0100

    fix

--- access/models.py
+++ access/models.py
@@ -1,34 +1,33 @@
 from django.db import models
 
 
-# Create your models here.
+# Роли пользователей
 class Role(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=True) #admin , user,...
 
     def __str__(self):
         return self.name
 
 
-class BiznessElement(models.Model):
-    code = models.CharField(max_length=50, unique=True)
-    discription = models.TextField()
+# Бизнес элемент
+class BusinessElement(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    code = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return self.code
+        return self.name
 
 
+# Правила ролей - доступ
 class AccessRule(models.Model):
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    element = models.ForeignKey(BiznessElement, on_delete=models.CASCADE)
-
+    element = models.ForeignKey(BusinessElement, on_delete=models.CASCADE)
+    
     read_permission = models.BooleanField(default=False)
     read_all_permission = models.BooleanField(default=False)
-
     create_permission = models.BooleanField(default=False)
-
     update_permission = models.BooleanField(default=False)
     update_all_permission = models.BooleanField(default=False)
-
     delete_permission = models.BooleanField(default=False)
     delete_all_permission = models.BooleanField(default=False)
 
