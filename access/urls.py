0aac38868648f54f659da40f86b538cefd549ecf
Nikita <nikitasizpl@gmail.com>
2026-01-15 02:34:05 +0100

    add path

--- access/urls.py
+++ access/urls.py
@@ -3,5 +3,6 @@ from .views import AccessRuleView, OrdersView
 
 urlpatterns = [
     path('rules/', AccessRuleView.as_view(), name='access_rules'),
+    path('rules/<int:rule_id>/', AccessRuleView.as_view(), name='access_rule_detail'),
     path('orders/', OrdersView.as_view(), name='orders'),
 ]
