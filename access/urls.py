ba7c9af8164884d11a787b92e3a1b4584a3a4ea5
Nikita <nikitasizpl@gmail.com>
2026-01-15 01:06:23 +0100

    del path

--- access/urls.py
+++ access/urls.py
@@ -3,6 +3,5 @@ from .views import AccessRuleView, OrdersView
 
 urlpatterns = [
     path('rules/', AccessRuleView.as_view(), name='access_rules'),
-    path('rules/<int:rule_id>/', AccessRuleView.as_view()),
     path('orders/', OrdersView.as_view(), name='orders'),
 ]
